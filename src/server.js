2b45e6cb45a8170f5ceb2a6f55c6b1b779fd9ce2
Max16 <max16@naver.com>
2022-03-15 09:37:46 +0900

    Frontend Setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000;
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+
+const PORT = 3000;
 const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
 
 app.listen(3000, handleListen);
